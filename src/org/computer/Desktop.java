5bc57de05d8b810c1be6991e6dde6a13686bd2bd
brightysharon17 <brightysharon17@gmail.com>
2024-08-26 10:56:24 +0530

    code is added

---
+++ src/org/computer/Desktop.java
@@ -0,0 +1,15 @@
+package org.computer;
+
+public class Desktop extends Computer {
+
+	private void desktopSize() {
+		System.out.println("12");
+
+	}
+	public static void main(String[] args) {
+		Desktop d = new Desktop();
+		d.desktopSize();
+		d.computerModel();
+	}
+	
+}
